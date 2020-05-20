d23713e265aebee82c387e208209cd7d578a88f7
Kamika-ZE <krysnn@gmail.com>
2020-05-21 00:41:40 +0200

    initial commit

---
+++ src/main/java/org/mickael/libraryclientweb/LibraryClientWebApplication.java
@@ -0,0 +1,13 @@
+package org.mickael.libraryclientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryClientWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryClientWebApplication.class, args);
+	}
+
+}
